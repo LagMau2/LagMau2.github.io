8f8ef13b6a89d7488d821d31e484ff568e6cfbad
LagMau2 <124909952+LagMau2@users.noreply.github.com>
2023-05-06 23:53:43 -0600

    Se crea pagina web

---
+++ cv.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+//http => (request,response)
+
+http.createServer((request,response)=>{
+    const file = request.url == '/' ? './cv.html' : `./${request.url}`;
+    
+    if(request.url == '/login'){
+        let data = [];
+        request.on('data', value => {
+            data.push(value);
+        }).on('end', ()=>{
+            let params = Buffer.concat(data).toString();
+            response.write(params);
+            response.end();
+        });
+    }
+    //const data = fs.readFileSync('./WWW/index.html');
+    fs.readFile(file,(error,data)=>{
+        if(error){
+            response.writeHead(404, {"Content-Type":"text/html"});
+            response.write("Not Found");
+            response.end(); 
+        }else{
+            const extension = request.url.split('.').pop(); //file.split('.').pop();
+            switch(extension){
+                case 'txt':
+                    response.writeHead(200, {"Content-Type":"text/plain"});
+                    break;
+                case 'html':
+                    response.writeHead(200, {"Content-Type":"text/html"});
+                    break;
+                case 'css':
+                    response.writeHead(200, {"Content-Type":"text/css"});
+                    break;
+                case 'jpeg':
+                    response.writeHead(200, {"Content-Type":"image/jpeg"});
+                    break;
+                default:
+                    response.writeHead(200, {"Content-Type":"text/html"});
+            };
+            response.write(data);
+            response.end();
+        };
+    });
+}).listen(4444);
